38cee6798603f432db6facc3d28f938f68186210
Fluffiest Floofers <thebluewulf@gmail.com>
2023-03-17 20:59:54 +0100

    up

--- lua/lambdaplayers/extaddon/server/lambdaplayers_playeradminsystem.lua
+++ lua/lambdaplayers/extaddon/server/lambdaplayers_playeradminsystem.lua
@@ -1,33 +1,4 @@
-local IsValid = IsValid
-local pairs = pairs
-local random = math.random
-local VectorRand = VectorRand
-local max = math.max
-local net = net
-local color_white = color_white
-local color_blue = Color( 0, 255, 0 )
-local color_admin = Color( 150, 25, 0 )
-
-
-
--- ------------- --
--- // CONVARS // --
--- ------------- --
-
-
-hook.Add( "LambdaOnConvarsCreated", "lambdaplayeradminsystemConvars", function()
-
-    CreateLambdaConvar( "lambdaplayers_pas_enabled", 1, true, false, false, "Enables the player administration system that allows player admin to do certain actions on Lambda Players using chat commands", 0, 1, { name = "Enable Player Admin System", type = "Bool", category = "Player Admin System" } )
-    CreateLambdaConvar( "lambdaplayers_pas_chatecho", 1, true, false, false, "Whenever the commands being used should be printed in the chat", 0, 1, { name = "Enable Commands Chat Print", type = "Bool", category = "Player Admin System" } )
-    CreateLambdaConvar( "lambdaplayers_pas_cmdprefix", ",", true, false, false, "The prefix used for chat commands. This will only accept one character!", nil, nil, { type = "Text", name = "Command Prefix", category = "Player Admin System" } )
-
-end )
-
-
-
-if SERVER then
-
-
+util.AddNetworkString("lambdaplayers_pas_chatprint")
 
 -- ---------------------- --
 -- // HELPER FUNCTIONS // --
@@ -392,5 +363,3 @@ hook.Add( "LambdaOnRemove", "lambdaplayers_pas_lambdaremove", function( lambda )
         gaggedLambdas[lambda:EntIndex()] = nil
     end
 end)
-
-end
